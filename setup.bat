f265463494c581970ef0eef6eff73fdf34e80bfa
Jay <j.patel8797@gmail.com>
2021-10-20 17:47:04 -0700

    Added setup.bat

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@ECHO Installing required packages for Stock-Notifier...
+
+@ECHO Installing Selenium...
+cmd /c "pip3 install selenium"
+
+@ECHO Installing dotENV...
+cmd /c "pip3 install python-dotenv"
+
+@ECHO Installing Sendgrid...
+cmd /c "pip3 install sendgrid"
+
+@ECHO Process finished 
+@pause
